92248a0450508d3ee4e4248b1929920169190295
gastonig9 <gastonig2020@gmail.com>
2023-10-17 19:30:08 -0300

    Componentes Header, Aseguradora, Contacto y Footer. Creacion de carpeta types

---
+++ src/assets/types/types.tsx
@@ -0,0 +1,3 @@
+export type Aseguradora = {
+    img: string;
+  };
--- src/pages/Inicio/Inicio.tsx
+++ src/pages/Inicio/Inicio.tsx
@@ -1,7 +1,15 @@
+import Aseguradoras from "./components/Aseguradoras/Aseguradoras";
+import Contacto from "./components/Contacto/Contacto";
+import Header from "./components/Header/Header";
+
 const Inicio = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Header />
+      <Aseguradoras />
+      <Contacto/>
+    </>
+  );
+};
 
-export default Inicio
+export default Inicio;
