889df036d2c1eccd546d16d064333727cc617805
JuanUbaldi <106642655+JuanUbaldi@users.noreply.github.com>
2023-10-23 16:52:22 -0300

    commit1

---
+++ src/Components/NavBar/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (<>
+    <div>
+      <ul>
+        <li>Inicio</li>
+        <li>Seguros</li>
+        <li>Siniestros</li>
+        <li>Contactame</li>
+      </ul>
+    </div>
+    <div>FC</div>
+    </>
+  );
+};
+
+export default Navbar;
