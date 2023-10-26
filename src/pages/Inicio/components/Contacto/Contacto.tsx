a738cf9cc628106649e41f5456dc5414467e6542
gastonig9 <gastonig2020@gmail.com>
2023-10-26 19:09:25 -0300

    Degradado en header y contactame componentes

--- src/pages/Inicio/components/Contacto/Contacto.tsx
+++ src/pages/Inicio/components/Contacto/Contacto.tsx
@@ -5,6 +5,7 @@ import "./Contacto.modules.css"
 const Contacto = () => {
   return (
     <section className="contactame-contain">
+        <div className='degrades-contacto'></div>
         <div className="contactame">
             <h1>Contactame</h1>
             <p>Si tenes alguna consulta, no dudes en contactarme.</p>
