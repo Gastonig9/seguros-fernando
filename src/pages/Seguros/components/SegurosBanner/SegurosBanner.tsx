672ae2c248092c496c06139d134ded4fd803764c
gastonig9 <gastonig2020@gmail.com>
2023-11-09 01:02:23 -0300

    Responsive en pagina de Inicio, Seguros y Contacto

--- src/pages/Seguros/components/SegurosBanner/SegurosBanner.tsx
+++ src/pages/Seguros/components/SegurosBanner/SegurosBanner.tsx
@@ -1,10 +1,15 @@
-import style from "./SegurosBanner.module.css"
+import style from "./SegurosBanner.module.css";
 const SegurosBanner = () => {
   return (
-    <div className={style.segurosBanner}>
+    <>
+      <div className={style.segurosMobile}>
+        <h1>Seguros</h1>
+      </div>
+      <div className={style.segurosBanner}>
         <h2>Seguros</h2>
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default SegurosBanner
+export default SegurosBanner;
