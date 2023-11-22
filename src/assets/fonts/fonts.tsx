34ea6bbe336117a7aa4588ec79ea7944fada47d8
gastonig9 <gastonig2020@gmail.com>
2023-11-22 02:49:22 -0300

    delete fonts

--- src/assets/fonts/fonts.tsx
+++
@@ -1,9 +0,0 @@
-import { Roboto } from 'google-fonts'
-
-
-export const roboto = Roboto({
-    weight: ['400', '500', '700'],
-    subsets: ['latin'],
-    variable: '--font-roboto',
-    display: 'swap',
-  });
